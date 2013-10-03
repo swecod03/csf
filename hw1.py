51e73e79d9f16b241c210047fb1a3173aff97048
swecod03 <swecod03@ccm-2610-b4.evergreen.edu>
2013-10-03 12:05:01 -0700

    completed problem 1

---
+++ hw1.py
@@ -0,0 +1,52 @@
+# Name: Cody Swearingen
+# Evergreen Login: swecod03
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+bit = math.sqrt((0-5.86)**2-4*8.5408)
+plus = (5.86 + bit) / 2
+minus = (5.86 - bit) / 2
+print "Value 1"
+print minus
+print "Value 2"
+print plus
+
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
