600fe30a60ca9185a1d08e5d6132afde552dc3ea
swecod03 <swecod03@ccm-2610-b4.evergreen.edu>
2013-10-31 11:43:03 -0700

    OH HEY IT WORKED THE WHOLE TIME
    I spent all of my time fixing it… and it wasn't broken...

---
+++ quiz2.py
@@ -0,0 +1,11 @@
+import lab2
+n = lab2.n
+
+if (n == 50):
+  print "equal to 50"
+elif (n > 50):
+  print "greater than 50"
+elif (n < 50):
+  print "less than 50"
+else:
+  print "DANGER, WILL ROBINSON! DANGER!"
