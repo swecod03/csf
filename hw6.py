4ab7be046467079233b9746e41a8a474e967cf03
Cody Alan Swearingen <swearingen.cody@gmail.com>
2013-11-15 16:38:06 -0800

    lkjdsafgsadg

---
+++ hw6.py
@@ -0,0 +1,74 @@
+# Name: Stephen Rancourt
+# Evergreen Login: ranste07
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 6
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+
+###
+### Problem 3
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 3 solution follows:"
+
+n = 'pumpkin pie'
+if n == 'apple pie':
+    print "I like apple pie."
+elif n == 'peach pie':
+    print "I hate peach pie."
+else:
+    print "I don't mind other types of pie"
+
+
+###
+### Problem 4
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 4 solution follows:"
+
+value = {'apple': 5, 'peach': 0, 'blueberry': 4, 'rhubarb': 1, 'pumpkin': 3, 'chocolate': 2}
+
+print value
+
+###
+### Problem 5
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 5 solution follows:"
+
+def valsearch(x, y):
+    print x[y]
+
+valsearch(value, 'blueberry')
+###
+### Problem 6
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 6 solution follows:"
+
+# I'm not sure what the question is even asking me to do. - Cody
+
+###
+### Problem 7
+###
+
+# DO NOT CHANGE THE FOLLOWING LINE
+print "Problem 7 solution follows:"
+
+# Of course problem 7 requires having done problem 6. What else. - Cody
+
+###
+### Collaboration
+###
+
+# ... List your collaborators and other sources of help here (websites, books, etc.),
+# ... as a comment (on a line starting with "#").
