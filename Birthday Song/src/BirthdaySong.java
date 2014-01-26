71b5278d4f6b279d032fe78b96a8109a3e6ba5dd
Cody Alan Swearingen <swearingen.cody@gmail.com>
2014-01-26 11:26:33 -0800

    Finished.

---
+++ Birthday Song/src/BirthdaySong.java
@@ -0,0 +1,15 @@
+
+public class BirthdaySong {
+	public static void main(String[] args){
+		String place;
+		String animal;
+		String verb;
+		place = "cave";
+		animal = "bear";
+		verb = "maul hikers";
+		
+		System.out.println("Happy birthday to you, \nYou live in a "+place+",\nYou look like a "+animal+",\nAnd you "+verb+" like one too.");
+	
+	}
+	
+}
